7e8ba40231476350aa6829253cabf4deaff94dc3
yunkepeng <yunke.peng@usys.ethz.ch>
2022-10-18 09:58:01 +0100

    make sure figs 3 and 4 are only dealing with forest

--- R/cal_nue.R
+++ R/cal_nue.R
@@ -37,67 +37,8 @@ cal_nue <- function(Tg_pred,PPFD_pred,vpd_pred,fAPAR_pred,age_pred,CNrt_pred,LMA
   bnf_f <- bnpp_f/root_cn_forest
 
   nuptake_f <- lnf_f + wnf_f + bnf_f
-  
-  #grass
-  npp_g <- summary(bp_grass_model)$coefficients[1,1] +  
-    summary(bp_grass_model)$coefficients[2,1] * log(PPFD_pred) +
-    summary(bp_grass_model)$coefficients[3,1] * Tg_pred 
-  
-  anpp_g <- npp_g* anpp_bp_grassland
-  bnpp_g <- npp_g-anpp_g
-  
-  leafnc_g <- 1/leaf_cn_grassland
-  
-  nre_g <- nre_grassland_constant
-  
-  lnf_g <- anpp_g*leafnc_g*(1-nre_g)
-  
-  bnf_g <- bnpp_g *(1/root_cn_grassland)
-  #41 is constant root c/n
-  nuptake_g <- lnf_g + bnf_g
-  
-  #combine into 2 pfts
-  npp_pft <- available_grid2* (npp_f*forest_percent +npp_g*grass_percent)
-  npp_forest <- available_grid2* (npp_f*forest_percent)
-  npp_grass <- available_grid2* (npp_g*grass_percent)
-  
-  anpp_pft <- available_grid2*(anpp_f*forest_percent +anpp_g*grass_percent)
-  anpp_forest <- available_grid2* (anpp_f*forest_percent)
-  anpp_grass <- available_grid2* (anpp_g*grass_percent)
-  
-  lnpp_forest <- available_grid2*lnpp_f*forest_percent
-  
-  wnpp_forest <- available_grid2*wnpp_f*forest_percent
-  
-  bnpp_pft <- available_grid2*(bnpp_f*forest_percent +bnpp_g*grass_percent)
-  bnpp_forest <- available_grid2* (bnpp_f*forest_percent)
-  bnpp_grass <- available_grid2* (bnpp_g*grass_percent)
-  
-  leafcn_pft <- 1/(available_grid2*(leafnc_f*forest_percent +leafnc_g*grass_percent))
-  leafcn_forest <- 1/available_grid2*leafnc_f*forest_percent
-  leafcn_grassland <- 1/available_grid2*leafnc_g*grass_percent
-  summary(leafcn_pft)
-  
-  nre_pft <- available_grid2*(nre_f*forest_percent +nre_g*grass_percent)
-  nre_forest <-  available_grid2*nre_f*forest_percent
-  nre_grassland <- available_grid2*nre_g*grass_percent
-  summary(nre_pft)
-  
-  lnf_pft <- available_grid2*(lnf_f*forest_percent +lnf_g*grass_percent)
-  lnf_forest <- available_grid2* (lnf_f*forest_percent)
-  lnf_grass <- available_grid2* (lnf_g*grass_percent)
-  
-  wnf_forest <- available_grid2*wnf_f*forest_percent
-  
-  bnf_pft <- available_grid2*(bnf_f*forest_percent +bnf_g*grass_percent)
-  bnf_forest <- available_grid2* (bnf_f*forest_percent)
-  bnf_grass <- available_grid2* (bnf_g*grass_percent)
-  
-  nuptake_pft <- available_grid2*(nuptake_f*forest_percent +nuptake_g*grass_percent)
-  nuptake_forest <- available_grid2* (nuptake_f*forest_percent)
-  nuptake_grass <- available_grid2* (nuptake_g*grass_percent)
-  
-  new_nue <-npp_pft/nuptake_pft
+
+  new_nue <-available_grid2*npp_f/nuptake_f
   
   nue_pft_ratio <- log(nue_final/new_nue)
   
